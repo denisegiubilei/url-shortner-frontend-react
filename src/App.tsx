1a48d7e730c6ffcd2b8c6799100ea3162ce9e2f5
Denise Giubilei Santos Filha <denise.filha@softplan.com.br>
2021-09-04 17:35:24 -0300

    fead: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header/Header";
 import { TopUrls } from "./components/TopUrls/TopUrls";
 import { UrlShortner } from "./components/UrlShortner/UrlShortner";
 
 function App() {
   return (
     <div>
-      <h1>Url shortner</h1>
+      <Header />
       <UrlShortner />
       <TopUrls />
     </div>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => (
+  <div>
+    <h1>Keep it short!</h1>
+    <p>make your urls easy to share</p>
+  </div>
+);
+
+export { Header }
