3b0c194607fcab3be4ddafd7372f962fa32fce43
Denise Giubilei <giubileidenise@gmail.com>
2021-09-05 14:18:32 -0300

    feat: add copy button

--- src/components/UrlShortner/ShortUrl/ShortUrl.tsx
+++ src/components/UrlShortner/ShortUrl/ShortUrl.tsx
@@ -4,8 +4,15 @@ interface ShortUrlProps {
   shortUrl: string;
 }
 
-export const ShortUrl = ({ shortUrl }: ShortUrlProps) => (
-  <p className={style.ShortUrl}>
-    Short url: <a href={shortUrl} rel="noreferrer" target="_blank">{shortUrl}</a>
-  </p>
-);
+export const ShortUrl = ({ shortUrl }: ShortUrlProps) => {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(shortUrl);
+  }
+
+  return (
+    <p className={style.ShortUrl}>
+      Short url: <a href={shortUrl} rel="noreferrer" target="_blank">{shortUrl}</a>
+      <button onClick={handleCopy}>Copy</button>
+    </p>
+  );
+}
