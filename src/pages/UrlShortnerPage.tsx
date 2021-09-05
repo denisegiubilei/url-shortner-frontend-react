e8dd5f63b842ccbaf547b1daee1865c44734e023
Denise Giubilei <giubileidenise@gmail.com>
2021-09-05 13:25:55 -0300

    style: add centered style to page container

--- src/pages/UrlShortnerPage.tsx
+++ src/pages/UrlShortnerPage.tsx
@@ -2,9 +2,11 @@ import { Header } from "../components/Header/Header";
 import { TopUrls } from "../components/TopUrls/TopUrls";
 import { UrlShortner } from "../components/UrlShortner/UrlShortner";
 
+import style from './UrlShortnerPage.module.scss'
+
 export function UrlShortnerPage() {
   return (
-    <div>
+    <div className={style.UrlShortnerPage}>
       <Header />
       <UrlShortner />
       <TopUrls />
