b3359720f95344dc7fb3b537b7c0ca5db3e5f6fd
Denise Giubilei <giubileidenise@gmail.com>
2021-09-05 13:20:32 -0300

    feat: add Loader while urls are fetching

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,5 @@
+import styles from "./Loader.module.scss";
+
+const Loader = () => <div className={styles.loader}></div>;
+
+export { Loader };
--- src/components/TopUrls/TopUrls.tsx
+++ src/components/TopUrls/TopUrls.tsx
@@ -2,16 +2,22 @@ import { useEffect, useState } from 'react';
 import { listTopVisited } from '../../api/urls';
 
 import { UrlEntity } from '../../interfaces/Url';
+import { Loader } from '../Loader/Loader';
 
 const TopUrls = () => {
-
-  const [urlList, setUrlList] = useState([] as UrlEntity[])
+  const [urlList, setUrlList] = useState([] as UrlEntity[]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     listTopVisited().then(res => {
       setUrlList(res);
+      setIsLoading(false);
     })
   }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <ul>
